ad27ba5fa0cf38586e09610012fcd0aa247e9c45
uniqueHRX <Roger.dey@outlook.com>
2025-06-16 19:35:32 +0800

    build: 🔧 添加并配置依赖项
    - 添加了Tailwind CSS依赖项并通过tailwindcss-primeui插件集成到项目
    - 添加pinia库、axios库依赖并导入项目
    - 配置unplugin-vue-components以实现PrimeVue的自动组件导入
    - 将PrimeVue、ToastService等模块导入到项目
    - 配置后端服务器和mock服务器的代理路径
    - 清理了多余的样式表
    - 更新了.gitignore
    
    ——黄润轩

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
 import './assets/main.css'
+import 'primeicons/primeicons.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import {createPinia} from "pinia"
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
 
 const app = createApp(App)
 
 app.use(router)
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark-mode-selector',
+    }
+  }
+})
+
+app.use(ToastService)
+app.use(ConfirmationService)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,45 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
+    Components({
+      resolvers: [
+        PrimeVueResolver(),
+      ]
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  base: '/',
+  // 设置反向代理实现跨域访问服务端
+  server: {
+    proxy: {
+      // mock服务器
+      '/mock': {
+        target: 'http://127.0.0.1:4523/m1/6398604-6095377-default',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/mock/, '')
+      },
+      // 开发服务器
+      '/dev': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/dev/, '')
+      }
+    }
   },
 })
