39512c011833c2a856c25e263d905cdb3be91c87
uniqueHRX <Roger.dey@outlook.com>
2025-07-04 22:41:28 +0800

    feat: 💄 添加色彩主题配置
    - 在`main.js`中添加了界面色彩主题配置
    - 主题色仅为暂定，后续可能更改
    
    ——黄润轩

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import ToastService from 'primevue/toastservice'
 import ConfirmationService from 'primevue/confirmationservice'
+import { definePreset } from '@primeuix/themes'
 
 const app = createApp(App)
 
@@ -19,12 +20,78 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 
+const Preset = definePreset(Aura, {
+  semantic: {
+    colorScheme: {
+      light: {
+        secondary: {
+          0: '#ffffff',
+          50: '{sky.50}',
+          100: '{sky.100}',
+          200: '{sky.200}',
+          300: '{sky.300}',
+          400: '{sky.400}',
+          500: '{sky.500}',
+          600: '{sky.600}',
+          700: '{sky.700}',
+          800: '{sky.800}',
+          900: '{sky.900}',
+          950: '{sky.950}'
+        },
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}'
+        }
+      },
+      dark: {
+        primary: {
+          50: '{indigo.50}',
+          100: '{indigo.100}',
+          200: '{indigo.200}',
+          300: '{indigo.300}',
+          400: '{indigo.400}',
+          500: '{indigo.500}',
+          600: '{indigo.600}',
+          700: '{indigo.700}',
+          800: '{indigo.800}',
+          900: '{indigo.900}',
+          950: '{indigo.950}'
+        },
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}'
+        }
+      }
+    }
+  }
+});
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: Preset,
     options: {
       darkModeSelector: '.dark-mode-selector',
-    }
+    },
   }
 })
 
