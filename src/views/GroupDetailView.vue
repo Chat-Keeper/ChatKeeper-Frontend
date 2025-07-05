bd367091b7ef87f71f80bcd0fd6e71ced0a648c1
uniqueHRX <Roger.dey@outlook.com>
2025-07-05 17:01:41 +0800

    feat: ✨ 实现群聊页面的请求和处理功能
    - 添加检索兴趣群成员功能
    - 实现文件上传及返回处理
    - 部分代码基于mock，生产环境需要修改
    
    ——黄润轩

--- src/views/GroupDetailView.vue
+++ src/views/GroupDetailView.vue
@@ -1,35 +1,115 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import request from '@/utils/request.js'
 
 export default defineComponent({
   name: 'GroupDetailView',
   props: {
     currentGroup: Object,
   },
+
   data() {
     return {
-      searchKey: "",
-    }
-  },
-  methods: {
-    upload(event) {
-      this.$toast.add({
-        severity: 'info',
-        summary: 'Success',
-        detail: 'File Uploaded',
-        life: 3000
-      });
+      findStr: '',
+      keyword: '',
+      uploadFile: null,
+      uploadProgress: 0,
+      uploadStatus: 'pending',
+      interestingSpeakers: []
     }
   },
 
+  methods: {
+    onSelect(event) {
+      this.uploadFile = event.files[0]
+      this.uploadProgress = 0
+      this.uploadStatus = 'pending'
+    },
+
+    onClear() {
+      this.uploadFile = null
+      this.uploadProgress = 0
+      this.uploadStatus = 'pending'
+    },
+
+    onUpload(event) {
+      this.uploadStatus = 'uploading'
+      this.uploadProgress = 0
+
+      const formData = new FormData()
+      formData.append('group_id', this.$route.params.group_id)
+      formData.append('chat_log', this.uploadFile)
+
+      request
+        .post('/data/upload', formData, {
+          onUploadProgress: (progressEvent) => {
+            this.uploadProgress = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+          },
+        })
+        .then((response) => {
+          console.log(response)
+          if (response.data.code === 200) {
+            console.log(response.data.msg)
+            this.uploadStatus = 'success'
+            this.uploadSuccessToast()
+            setTimeout(() => this.$refs.fileUploadRef.clear(), 3000)
+            this.$emit('updateGroup', response.data.data)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          this.uploadStatus = 'error'
+        })
+    },
+
+    uploadSuccessToast() {
+      this.$toast.add({
+        severity: 'success',
+        summary: '上传成功',
+        detail: '文件已经上传至数据库，请等待页面刷新……',
+        life: 3000,
+      })
+    },
+
+    searchInterest() {
+      request
+        .post('/analysis/search', {
+          keyword: this.keyword,
+        })
+        .then((response) => {
+          console.log(response)
+          if (response.data.code === 200) {
+            console.log(response.data.msg)
+            if (response.data.data.speaker_info) {
+              this.interestingSpeakers = response.data.data.speaker_info
+              this.interestingSpeakers.sort((a, b) => b.relativity - a.relativity)
+            }
+            this.keyword = ''
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+
+  watch: {
+    $route() {
+      this.findStr = ''
+      this.keyword = ''
+      this.uploadFile = null
+      this.uploadProgress = 0
+      this.uploadStatus = 'pending'
+      this.interestingSpeakers = []
+      this.$refs.fileUploadRef.clear()
+    },
+  },
 })
 </script>
 
 <template>
   <div class="lg:flex items-start p-5">
-
     <div class="lg:w-7/12 w-11/12 flex-col">
-
       <Card class="dark:bg-surface-900! m-3">
         <template #title>
           <div class="flex items-center gap-3 mb-1">
@@ -52,7 +132,10 @@ export default defineComponent({
             <div class="flex items-center gap-2 mt-2">
               <div class="flex items-center gap-2">
                 <i class="pi pi-calendar"></i>
-                <p>时间范围：{{ currentGroup.start_time.slice(0,10) }} ~ {{ currentGroup.end_time.slice(0,10) }}</p>
+                <p>
+                  时间范围：{{ currentGroup.start_time.slice(0, 10) }} ~
+                  {{ currentGroup.end_time.slice(0, 10) }}
+                </p>
               </div>
             </div>
           </div>
@@ -66,52 +149,65 @@ export default defineComponent({
               <Avatar icon="pi pi-user" shape="circle"></Avatar>
               <p>群聊成员</p>
             </div>
-            <IconField >
+            <IconField>
               <InputIcon class="pi pi-search" />
-              <InputText v-model="searchKey" placeholder="搜索群成员" size="small" />
+              <InputText v-model="findStr" placeholder="搜索群成员" size="small" />
             </IconField>
           </div>
         </template>
         <template #content>
           <div v-for="speaker in currentGroup.speaker_list" class="flex-col">
-            <router-link :to="'/home/speakers/' + speaker.speaker_id" v-if="searchKey === '' || speaker.speaker_name.includes(searchKey)">
+            <router-link
+              :to="'/home/speakers/' + speaker.speaker_id"
+              v-if="
+                findStr === '' ||
+                speaker.speaker_name.includes(findStr) ||
+                speaker.speaker_qq.includes(findStr)
+              "
+            >
               <Card
                 class="my-2 shadow-none!"
-                :class="speaker.analyzed
-                  ? 'bg-primary-50! hover:bg-primary-100! dark:bg-primary-950! dark:hover:bg-primary-900! text-primary-500! dark:text-primary-300!'
-                  : 'bg-surface-100! hover:bg-surface-200! dark:bg-surface-800! dark:hover:bg-surface-700! text-surface-400! dark:text-surface-400!'"
+                :class="
+                  speaker.analyzed
+                    ? 'bg-primary-50! hover:bg-primary-100! dark:bg-primary-950! dark:hover:bg-primary-900! text-primary-500! dark:text-primary-300!'
+                    : 'bg-surface-100! hover:bg-surface-200! dark:bg-surface-800! dark:hover:bg-surface-700! text-surface-400! dark:text-surface-400!'
+                "
               >
                 <template #title>
                   <div class="flex gap-4 items-center text-lg -m-2">
                     <Badge
                       size="xlarge"
                       class="min-w-5/12! mr-2"
-                      :class="speaker.analyzed
-                        ? ''
-                        : 'bg-surface-400! dark:bg-surface-400!'"
+                      :class="speaker.analyzed ? '' : 'bg-surface-400! dark:bg-surface-400!'"
                     >
                       <p>{{ speaker.speaker_name }}</p>
                     </Badge>
                     <div class="flex gap-2 items-center">
                       <i class="pi pi-tag"></i>
-                      <p size="large" class=" font-bold" >
+                      <p size="large" class="font-bold">
                         {{ speaker.speaker_qq }}
                       </p>
                     </div>
-
                   </div>
                 </template>
                 <template #content>
                   <div class="-m-2 mt-3">
-                    <div class="flex items-center gap-3 mr-2">
-                      <i class="pi pi-send"></i>
+                    <div class="flex items-center gap-3 m-1">
+                      <div class="flex items-center gap-2">
+                        <i class="pi pi-send"></i>
+                        <p>消息数量</p>
+                      </div>
                       <MeterGroup
-                        :value="[{
-                          label: '',
-                          value: (speaker.speaker_msg_freq / currentGroup.max_msg_freq * 100),
-                          color: speaker.analyzed ? 'var(--p-primary-color)' : 'var(--p-surface-400)'
-                        }]"
-                        class="w-9/12"
+                        :value="[
+                          {
+                            label: '',
+                            value: (speaker.speaker_msg_freq / currentGroup.max_msg_freq) * 100,
+                            color: speaker.analyzed
+                              ? 'var(--p-primary-color)'
+                              : 'var(--p-surface-400)',
+                          },
+                        ]"
+                        class="w-8/12"
                         :pt="{
                           labelList: { class: 'hidden!' },
                         }"
@@ -126,11 +222,9 @@ export default defineComponent({
           </div>
         </template>
       </Card>
-
     </div>
 
     <div class="lg:w-5/12 w-11/12 flex-col">
-
       <Card class="dark:bg-surface-900! m-3">
         <template #title>
           <div class="flex items-center gap-3 mb-1">
@@ -143,12 +237,71 @@ export default defineComponent({
             在搜索框中输入你的“兴趣关键词”，我们将为你发现你可能感兴趣的群聊成员，快来试试吧！
           </div>
           <div class="flex items-start justify-between mt-3">
-            <InputText v-model="value" placeholder="兴趣关键词" class=" w-[calc(100%-100px)]"/>
-            <Button @click="">
+            <InputText v-model="keyword" placeholder="兴趣关键词" class="w-[calc(100%-100px)]" />
+            <Button @click="searchInterest()">
               <i class="pi pi-lightbulb"></i>
               <p>发现</p>
             </Button>
           </div>
+
+          <div v-for="speaker in interestingSpeakers" class="flex-col mt-5">
+            <router-link :to="'/home/speakers/' + speaker.speaker_id">
+              <Card
+                class="my-2 shadow-none!"
+                :class="
+                  speaker.analyzed
+                    ? 'bg-primary-50! hover:bg-primary-100! dark:bg-primary-950! dark:hover:bg-primary-900! text-primary-500! dark:text-primary-300!'
+                    : 'bg-surface-100! hover:bg-surface-200! dark:bg-surface-800! dark:hover:bg-surface-700! text-surface-400! dark:text-surface-400!'
+                "
+              >
+                <template #title>
+                  <div class="flex gap-4 items-center text-lg -m-2">
+                    <Badge
+                      size="xlarge"
+                      class="min-w-5/12! mr-2"
+                      :class="speaker.analyzed ? '' : 'bg-surface-400! dark:bg-surface-400!'"
+                    >
+                      <p>{{ speaker.speaker_name }}</p>
+                    </Badge>
+                    <div class="flex gap-2 items-center">
+                      <i class="pi pi-tag"></i>
+                      <p size="large" class="font-bold">
+                        {{ speaker.speaker_qq }}
+                      </p>
+                    </div>
+                  </div>
+                </template>
+                <template #content>
+                  <div class="-m-2 mt-3">
+                    <div class="flex items-center gap-3 m-1">
+                      <div class="flex items-center gap-2">
+                        <i class="pi pi-thumbs-up"></i>
+                        <p>兴趣度</p>
+                      </div>
+                      <MeterGroup
+                        :value="[
+                          {
+                            label: '',
+                            value: speaker.relativity,
+                            color: speaker.analyzed
+                              ? 'var(--p-primary-color)'
+                              : 'var(--p-surface-400)',
+                          },
+                        ]"
+                        class="w-7/12"
+                        :pt="{
+                          labelList: { class: 'hidden!' },
+                        }"
+                      >
+                      </MeterGroup>
+                      <p class="font-bold text-sm">{{ speaker.relativity }}</p>
+                    </div>
+                  </div>
+                </template>
+              </Card>
+            </router-link>
+          </div>
+
         </template>
       </Card>
 
@@ -162,14 +315,19 @@ export default defineComponent({
         <template #content>
           <div class="flex items-start mt-2">
             <FileUpload
+              ref="fileUploadRef"
+              custom-upload
+              name="chat_log"
+              @select="onSelect"
+              @remove="onClear"
+              @clear="onClear"
+              @uploader="onUpload($event)"
+              accept=".txt"
+              :max-file-size="100 * 1024 * 1024"
+              :multiple="false"
               choose-label="选择"
               upload-label="上传"
               cancel-label="取消"
-              name="demo[]"
-              url="/api/upload"
-              @upload="upload($event)"
-              accept=".txt"
-              :maxFileSize="1000000"
               :pt="{
                 root: { class: 'w-full!' },
                 content: { class: 'min-h-15' },
@@ -177,11 +335,25 @@ export default defineComponent({
                 pcUploadButton: { root: { class: 'p-button-outlined p-button-success' } },
                 pcCancelButton: { root: { class: 'p-button-outlined p-button-danger' } },
                 fileThumbnail: { class: 'hidden!' },
-                pcFileBadge: { root: { variant: 'outlined' } },
+                pcFileRemoveButton: { root: { class: 'hidden!' } },
+                pcProgressBar: {
+                  value: () => ({ style: { width: uploadProgress + '%' } }),
+                },
+                file: {
+                  class:
+                    uploadStatus === 'success'
+                      ? 'border-l-3 border-green-500'
+                      : uploadStatus === 'error'
+                        ? 'border-l-3 border-red-500'
+                        : 'border-l-3 border-yellow-500',
+                },
+                pcFileBadge: { root: { class: 'hidden!' } },
               }"
             >
               <template #empty>
-                <p class="text-surface-500! dark:text-surface-300!">请点击按钮或者将文件拖动到这里</p>
+                <p class="text-surface-500! dark:text-surface-300!">
+                  请点击按钮或者将文件拖动到这里
+                </p>
               </template>
             </FileUpload>
           </div>
@@ -204,11 +376,8 @@ export default defineComponent({
           </div>
         </template>
       </Card>
-
     </div>
-
   </div>
-
 </template>
 
 <style scoped></style>
--- src/views/GroupsView.vue
+++ src/views/GroupsView.vue
@@ -1,6 +1,5 @@
 <script>
 import { defineComponent } from 'vue'
-import { useAuthStore } from '@/store/auth.js'
 import SideMenu from '@/components/SideMenu.vue'
 import request from '@/utils/request.js'
 import GroupDetailView from '@/views/GroupDetailView.vue'
@@ -10,47 +9,55 @@ export default defineComponent({
   components: { GroupDetailView, SideMenu },
   data() {
     return {
-      authStore: null,
       groupNum: 0,
       groupList: [],
       currentGroup: null,
     }
   },
+
   methods: {
     getGroups() {
-      request.get('/data/group/list', {
-        user_id: this.authStore.userId,
-        token: this.authStore.token,
-      })
-      .then((response) => {
-        console.log(response)
-        if (response.data.code === 200) {
-          this.groupNum = response.data.data.group_num
-          this.groupList = response.data.data.group_list
-          console.log(response.data.msg)
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+      request
+        .get('/data/group/list')
+        .then((response) => {
+          console.log(response)
+          if (response.data.code === 200) {
+            this.groupNum = response.data.data.group_num
+            this.groupList = response.data.data.group_list
+            console.log(response.data.msg)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+
+    updateGroup(data) {
+      // mock返回group_id随机时的临时策略，后续修改
+      let index = this.groupList.findIndex((group) => group.group_id === this.$route.params.group_id)
+      if (index !== -1) {
+        this.groupList[index] = data
+      }
+      this.$router.push('/home/groups/' + this.groupList[index].group_id)
     }
   },
+
   created() {
-    this.authStore = useAuthStore()
     this.getGroups()
   },
+
   watch: {
-    '$route'(to, from) {
+    $route(to, from) {
       console.log(to.params.group_id)
-      this.currentGroup = this.groupList.find(group => group.group_id === to.params.group_id)
+      this.currentGroup = this.groupList.find((group) => group.group_id === to.params.group_id)
       if (!this.currentGroup) {
         this.$router.push('/home/groups')
         console.log('not found')
       }
       this.currentGroup.speaker_list.sort((a, b) => b.speaker_msg_freq - a.speaker_msg_freq)
       this.currentGroup['max_msg_freq'] = this.currentGroup.speaker_list[0].speaker_msg_freq
-    }
-  }
+    },
+  },
 })
 </script>
 
@@ -60,7 +67,7 @@ export default defineComponent({
       <SideMenu :group-list="groupList" class="h-[calc(100vh-4rem)]"></SideMenu>
     </div>
     <div class="flex-grow min-h-[calc(100vh-4rem)] bg-surface-50 dark:bg-surface-950">
-      <router-view :current-group="currentGroup"></router-view>
+      <router-view :current-group="currentGroup" @updateGroup="updateGroup"></router-view>
     </div>
   </div>
 </template>
