29de077cf7645bd13d09e6bca91a745f1b738ade
uniqueHRX <Roger.dey@outlook.com>
2025-07-05 16:58:24 +0800

    refactor: 🦺 采用更安全的请求格式
    - 将令牌放在请求头中发送，提高安全性
    
    ——黄润轩

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,23 @@
 import axios from 'axios'
+import { useAuthStore } from '@/store/auth.js'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_BASE,
   timeout: 10000,
 })
+
+request.interceptors.request.use(
+  config => {
+    const token = useAuthStore().token
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    console.log(config)
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
 
 export default request
